e893d1039d38841c68abc5af16182545578c3e6f
Arjun Ghimire <developerarjun1@gmail.com>
2021-11-17 12:42:51 +0545

    task 3 completed

---
+++ Task 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int countInput(int[] arr)
+        {
+            int count = 0;
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int x = arr[i];
+                int add = x + 1;
+                bool istrue = false;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] == add)
+                    {
+                        istrue = true;
+                        break;
+                    }
+                }
+                for (int k = i - 1;
+                    !istrue && k >= 0; k--)
+                {
+                    if (arr[k] == add)
+                    {
+                        istrue = true;
+                        break;
+                    }
+                }
+                if (istrue == true)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            Console.WriteLine(countInput(arr));
+        }
+    }
+}
