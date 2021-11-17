8014509c53befb82e89072787ba34d85db492557
Arjun Ghimire <developerarjun1@gmail.com>
2021-11-18 00:11:27 +0545

    task 2 completed

---
+++ Task 2/WebApplication1/WebApplication1/Controllers/RosterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.GroupHub;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RosterController : ControllerBase
+    {
+        public class UserDetails
+        {
+            public string User { get; set; }
+        }
+        private IHubContext<ChatHub> HubContext{ get; set; }
+        public RosterController(IHubContext<ChatHub> hubcontext)
+        {
+            HubContext = hubcontext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddToRosterGroup([FromBody] UserDetails user)
+        {
+            await HubContext.Clients.All.SendAsync("ReceiveMessage", user.User);
+            return Ok($"{user.User} Has Add To Group");
+        }
+    }
+}
---
+++ Task 2/WebApplication1/WebApplication1/GroupHub/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+    
+namespace WebApplication1.GroupHub
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinRoster(string user)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user);
+        }
+    }
+}
